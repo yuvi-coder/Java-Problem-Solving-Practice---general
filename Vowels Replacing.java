7da1b73613b24a3fe9016aeb29a691fbd7345af5
Yuvi_Developer <81914878+yuvi-coder@users.noreply.github.com>
2025-04-22 22:51:15 +0530

    Create Vowels Replacing.java

---
+++ Vowels Replacing.java
@@ -0,0 +1,17 @@
+// Replacing the vowels with other string elements or characters
+import java.io*;
+import java.util.*;
+public class main{
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+        String input=sc.nextLine();
+        String result="";
+        for(int i=0;i<input.length();i++){
+            char ch=input.charAt(i);
+             if(ch=='a'||ch=='A'||ch=='e'||ch=='E'||ch=='i'||ch=='I'||ch=='o'||ch=='O'||ch=='u'||ch=='U'){
+                 result+=
+             }
+            
+        }
+    }
+}
